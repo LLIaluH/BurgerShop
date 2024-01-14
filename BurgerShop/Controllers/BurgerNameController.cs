0e75b76ed36649d3c853d3baf2b6e8d38c310ad1
LLIaluH <71131369+LLIaluH@users.noreply.github.com>
2024-01-14 16:32:38 +0300

    BurgerNameController and View

---
+++ BurgerShop/Controllers/BurgerNameController.cs
@@ -0,0 +1,19 @@
+﻿using BurgerShop.AppClases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerShop.Controllers
+{
+    public class BurgerNameController : Controller
+    {
+        private readonly BurgerShopContext _context;
+        public BurgerNameController(BurgerShopContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            var burgers = _context.BurgerNames.ToList();
+            return View(burgers);
+        }
+    }
+}
---
+++ BurgerShop/Migrations/20240114123239_UpdateNames.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BurgerShop.Migrations
+{
+    public partial class UpdateNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageSrc",
+                table: "BurgerNames",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageSrc",
+                table: "BurgerNames");
+        }
+    }
+}
