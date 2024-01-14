e3189e074c4ff8814b24fe4e58d2bc79615ff6de
LLIaluH <71131369+LLIaluH@users.noreply.github.com>
2024-01-14 15:16:33 +0300

    поле картинки бургера

--- BurgerShop/Models/BurgerName.cs
+++ BurgerShop/Models/BurgerName.cs
@@ -8,5 +8,6 @@ namespace BurgerShop.Models
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public float Price { get; set; }
+        public string? ImageSrc { get; set; }
     }
 }
